c3c8475246ae148c1456a65f7a3660a4bd662271
Prashu <1707prash@gmail.com>
2021-04-02 14:39:03 +0530

    invit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'screens/templates.dart';
 void main() {
   runApp(MyApp());
 }
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Templates(),
     );
   }
 }
---
+++ lib/screens/templates.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Templates extends StatefulWidget {
+  @override
+  _TemplatesState createState() => _TemplatesState();
+}
+
+class _TemplatesState extends State<Templates> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+        body: Container(
+          child: Text(' i ')
+        ),
+      ),
+    );
+  }
+}
